f6a303af93e6cfbd53c69ec7eb5c39cb37ab99ec
Mumtaz.Khan <mumtaz.khan@daugherty.com>
2018-09-22 00:33:05 -0500

    Fixed repository tests

--- src/main/java/com/tdd/testbeforeanything/repository/CarRepository.java
+++ src/main/java/com/tdd/testbeforeanything/repository/CarRepository.java
@@ -3,9 +3,10 @@ package com.tdd.testbeforeanything.repository;
 import com.tdd.testbeforeanything.domain.Car;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface CarRepository extends CrudRepository<Car, Long> {
 
-    @Query("select car from Car where car.name = :name")
-    public Car findByName(String prius);
+    @Query("select car from Car car where car.name = :name")
+    public Car findByName(@Param("name") String name);
 }
